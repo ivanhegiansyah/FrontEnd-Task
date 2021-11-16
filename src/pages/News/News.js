ce74b55739e1be7ae133473c58010dfe4b6205b7
Ivan Hegiansyah <57437014+ivanhegiansyah@users.noreply.github.com>
2021-11-16 11:43:17 +0800

    Delete src directory

--- src/pages/News/News.js
+++
@@ -1,9 +0,0 @@
-import NewsList from "./NewsList";
-
-export default function News() {
-  return (
-    <div>
-      <NewsList />
-    </div>
-  );
-}
--- src/pages/News/NewsItem.js
+++
@@ -1,9 +0,0 @@
-export default function NewsItem(props) {
-  const { content, title } = props;
-  return (
-    <div className="container card p-2 mb-2">
-      <h5>{title}</h5>
-      <p>{content}</p>
-    </div>
-  );
-}
